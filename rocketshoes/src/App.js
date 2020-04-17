8f321412b5ae8e70b94d346ebaf15033583f53dd
Milene Taborda <milenetaborda1@outlook.com>
2020-04-17 16:43:43 -0300

    Estilização da Home

--- rocketshoes/src/App.js
+++ rocketshoes/src/App.js
@@ -2,14 +2,17 @@ import React from 'react';
 
 import { BrowserRouter } from 'react-router-dom';
 
+import GlobalStyle from './styles/global'
+
 import Routes from './routes';
+import Header from './components/Header'
 
 
 function App() {
   return (
     <BrowserRouter>
-    {/* <Header /> */}
-    
+    <Header />
+    <GlobalStyle />
     <Routes />
     </BrowserRouter>
   );
--- rocketshoes/src/pages/Home/index.js
+++ rocketshoes/src/pages/Home/index.js
@@ -1,9 +1,112 @@
 import React from 'react';
+import { MdShoppingBasket } from 'react-icons/md'
 
-// import { Container } from './styles';
+import { ProductList } from './styles';
 
 export default function Home() {
   return (
-    <div />
+    <ProductList>
+      <li>
+        <img src="https://http2.mlstatic.com/novo-tenis-vans-old-skool-classic-unissex-promoco-original-D_NQ_NP_894541-MLB28422969325_102018-F.jpg"
+        alt="Tênis"
+        />
+  
+      <strong>Tenis muito legal</strong>
+      <span>R$: 120,90</span>
+
+      <button type="button">
+        <div>
+          <MdShoppingBasket size={16} color="#fff"/> 3
+        </div>
+
+        <span>ADICIONAR AO CARRINHO</span>
+      </button>
+      </li>
+
+      <li>
+        <img src="https://http2.mlstatic.com/novo-tenis-vans-old-skool-classic-unissex-promoco-original-D_NQ_NP_894541-MLB28422969325_102018-F.jpg"
+        alt="Tênis"
+        />
+  
+      <strong>Tenis muito legal</strong>
+      <span>R$: 120,90</span>
+
+      <button type="button">
+        <div>
+          <MdShoppingBasket size={16} color="#fff"/> 3
+        </div>
+
+        <span>ADICIONAR AO CARRINHO</span>
+      </button>
+      </li>
+
+      <li>
+        <img src="https://http2.mlstatic.com/novo-tenis-vans-old-skool-classic-unissex-promoco-original-D_NQ_NP_894541-MLB28422969325_102018-F.jpg"
+        alt="Tênis"
+        />
+  
+      <strong>Tenis muito legal</strong>
+      <span>R$: 120,90</span>
+
+      <button type="button">
+        <div>
+          <MdShoppingBasket size={16} color="#fff"/> 3
+        </div>
+
+        <span>ADICIONAR AO CARRINHO</span>
+      </button>
+      </li>
+
+      <li>
+        <img src="https://http2.mlstatic.com/novo-tenis-vans-old-skool-classic-unissex-promoco-original-D_NQ_NP_894541-MLB28422969325_102018-F.jpg"
+        alt="Tênis"
+        />
+  
+      <strong>Tenis muito legal</strong>
+      <span>R$: 120,90</span>
+
+      <button type="button">
+        <div>
+          <MdShoppingBasket size={16} color="#fff"/> 3
+        </div>
+
+        <span>ADICIONAR AO CARRINHO</span>
+      </button>
+      </li>
+
+      <li>
+        <img src="https://http2.mlstatic.com/novo-tenis-vans-old-skool-classic-unissex-promoco-original-D_NQ_NP_894541-MLB28422969325_102018-F.jpg"
+        alt="Tênis"
+        />
+  
+      <strong>Tenis muito legal</strong>
+      <span>R$: 120,90</span>
+
+      <button type="button">
+        <div>
+          <MdShoppingBasket size={16} color="#fff"/> 3
+        </div>
+
+        <span>ADICIONAR AO CARRINHO</span>
+      </button>
+      </li>
+
+      <li>
+        <img src="https://http2.mlstatic.com/novo-tenis-vans-old-skool-classic-unissex-promoco-original-D_NQ_NP_894541-MLB28422969325_102018-F.jpg"
+        alt="Tênis"
+        />
+  
+      <strong>Tenis muito legal</strong>
+      <span>R$: 120,90</span>
+
+      <button type="button">
+        <div>
+          <MdShoppingBasket size={16} color="#fff"/> 3
+        </div>
+
+        <span>ADICIONAR AO CARRINHO</span>
+      </button>
+      </li>
+    </ProductList>
   );
 }
