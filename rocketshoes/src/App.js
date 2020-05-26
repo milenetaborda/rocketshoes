ad8846fc6c19101369a5c17e9e102a1b9fb4ef6a
Milene Taborda <milenetaborda1@outlook.com>
2020-05-26 12:54:51 -0300

    Verificação de estoque

--- rocketshoes/src/App.js
+++ rocketshoes/src/App.js
@@ -1,10 +1,9 @@
 import React from 'react';
-
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import {ToastContainer} from 'react-toastify';
 
 import './config/ReactotronConfig';
-
 import GlobalStyle from './styles/global'
 
 import Routes from './routes';
@@ -16,10 +15,11 @@ function App() {
   return (
   <Provider store={store}>
     <BrowserRouter>
-    <Header />
-    <GlobalStyle />
-    <Routes />
-    </BrowserRouter>
+      <Header />
+        <GlobalStyle />
+        <Routes />
+        <ToastContainer autoClose={3000} />
+      </BrowserRouter>
   </Provider>
   );
 }
